cd5e60f2d93b6d05c4dfca4f2e3dd66013a49510
bergstenarn <andersbergsten@yahoo.se>
2022-04-24 22:44:37 +0300

    282. Modularizing Large GraphQL Projects: Resolvers

--- server.js
+++ server.js
@@ -5,28 +5,12 @@ const { loadFilesSync } = require("@graphql-tools/load-files");
 const { makeExecutableSchema } = require("@graphql-tools/schema");
 
 const typesArray = loadFilesSync(path.join(__dirname, "**/*.graphql"));
+const resolversArray = loadFilesSync(path.join(__dirname, "**/*.resolvers.js"));
 
 const schema = makeExecutableSchema({
   typeDefs: typesArray,
-  resolvers: {
-    Query: {
-      products: async (parent) => {
-        console.log("Getting products ...");
-        const product = await Promise.resolve(parent.products);
-        return product;
-      },
-      orders: (parent) => {
-        console.log("Getting orders ...");
-        return parent.orders;
-      },
-    },
-  },
+  resolvers: resolversArray,
 });
-
-const root = {
-  products: require("./products/products.model"),
-  orders: require("./orders/orders.model"),
-};
 
 const app = express();
 
@@ -34,7 +18,6 @@ app.use(
   "/graphql",
   graphqlHTTP({
     schema,
-    rootValue: root,
     graphiql: true,
   })
 );
